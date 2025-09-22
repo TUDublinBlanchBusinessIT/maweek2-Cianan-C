94734349735632b156b81e5b1b2f117cbd8275a7
Cianan <B00164511@mytudublin.ie>
2025-09-22 16:32:57 +0100

    Completed Part 2

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import {StyleSheet, Text, View } from 'react-native';
 // You can import supported modules from npm
 import { Card } from 'react-native-paper';
 
-// or any files within the Snacks
+// or any files within the Snack
 import AssetExample from './components/AssetExample';
 
 export default function App() {
