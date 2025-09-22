4ef69a9b32a736bcd62301090b320dc1413ac291
Cianan <B00164511@mytudublin.ie>
2025-09-22 16:28:29 +0100

    test commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import {StyleSheet, Text, View } from 'react-native';
+
+// You can import supported modules from npm
+import { Card } from 'react-native-paper';
+
+// or any files within the Snacks
+import AssetExample from './components/AssetExample';
+
+export default function App() {
+  return (
+    <View>
+      <Text>Hello, World</Text>
+    </View>
+  );
+};
+
+
